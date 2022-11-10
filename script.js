a87f6f4ef504b2569e5f119c69bd1e9d64bb7d41
Stephen Brown <stephenrbrownnb@gmail.com>
2022-11-10 17:23:29 -0400

    Added User selections

--- script.js
+++ script.js
@@ -9,8 +9,41 @@ let possibleCharacters=[];
 let password = [];
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Password Criteria 
 function writePassword() {
+  let passwordLength = prompt("How long should your password be? (Please choose a number between 8 and 128");
+   if (passwordLength < 8) {alert("You must have at least 8 characters");generatePassword();
+
+   }
+   else if (passwordLength > 128) {alert("You cannot have more than 128 characters.");generatePassword();
+
+  }
+  let cLower = confirm("Should your password contain Lower case letters? (Ok for Yes, Cancel for No)");
+    if (cLower) {
+  possibleCharacters = possibleCharacters.concat(lowerCase);
+}
+  let cUpper = confirm("Should your password contain Upper case Letters? (Ok for Yes, Cancel for No)");
+    if (cUpper) {
+    possibleCharacters = possibleCharacters.concat(upperCase);
+  } 
+  
+  let cNumber = confirm("Should your password contain Numbers? (Ok for Yes, Cancel for No.");
+    if (cNumber) {
+    possibleCharacters = possibleCharacters.concat(numbers);
+  }
+  
+  let cSymbol = confirm("Should your password contain Symbols? (Ok for Yes, Cancel for No.");
+    if (cSymbol) {
+    possibleCharacters = possibleCharacters.concat(symbols);
+   }
+
+   else if (possibleCharacters === ""){alert("You must choose at least one criteria for password");}
+
+
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
